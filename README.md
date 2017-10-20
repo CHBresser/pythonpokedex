9c24c829f16e0656d071137b8f7a771cffcb6a77
Unknown <zachbresser@gmail.com>
2017-10-20 19:58:40 -0400

    Clarification and Error Checking

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pythonpokedex
 Python Interface for veekun-pokedex
+
+
+Python Pokedex uses a 50MB SQLite3 Database called veekun-pokedex by user veekun:
+
+Website: https://veekun.com
+Github: https://github.com/veekun
+
+While Veekun's site is perfect for all your pokemon knowledge, I used it as a project starting in Oct 2017 to build a python interface for it. 
+
+PythonPokedex is a Work in Progress piece currently on version 0.1, it is a text based interface for the DB
+
+Plans for v1.0:
+
+Web interface using Python's Flash framework to access the information in a more user friendly manner.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
-from prettytable import PrettyTable
+from prettytable import PrettyTable, from_db_cursor
 
+# Force user to provide table name
 while True:
     qTable = input("What table would you like?")
     if(qTable):
@@ -13,7 +14,7 @@ qid = input("What are we looking for?")
 
 def queryDB(table, column, id):
 
-    # Declare Variables
+    # Declare Function Variables
     sqlite_file = 'veekun-pokedex.sqlite'
     queried_table = table
     id_column = column
@@ -21,53 +22,74 @@ def queryDB(table, column, id):
     listHeaders = []
     pokemonID = []
     abilityList = []
-
+    pokemonAList = []
+    headerList = ['pokemon.identifier', 'pokemon.height', 'pokemon.weight', 'abilities.identifier']
     # Connect to SQLite3 Database
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
 
+    # Grab Table Headers
     c.execute("PRAGMA table_info({tn})".\
         format(tn=queried_table))
     columns = c.fetchall()
     columns = list(columns)
     
-    # Query and format output
+    # Previous command has a lot of useless information, filter out just the header info we need
     for column in columns:
         listHeaders.append(column[1])
 
+    # If all earlier prompts were answered.
     if(qTable and qColumn and qid):
         c.execute("SELECT * FROM {tn} WHERE {cn} LIKE '{id}'".\
-            format(tn=queried_table, cn=id_column, id=some_id))
+           format(tn=queried_table, cn=id_column, id=some_id))
         all_rows = c.fetchall()
+    # If only table was provided. (Table is required, and forced ;)
     else:
         c.execute("SELECT * FROM {tn}".\
             format(tn=queried_table))
         all_rows = c.fetchall()
+        
+    # Create Table object with appropriate headers
     x = PrettyTable(listHeaders)
-    # Add all rows to table
+    # Add all rows to table iteratively 
     for row in all_rows:
         x.add_row(row)
         if(table == "pokemon"):
             pokemonID.append(row[0])
-    
+    # "Display" table
     print(x)
-    
-    print("Abilities: ")
-    for id in pokemonID:
-        # Grab the pokemon name and ability name using the id #'s from pokemon_abilities
-        c.execute('''SELECT pokemon.identifier, abilities.identifier  
-            FROM pokemon_abilities
-            INNER JOIN abilities ON abilities.id = pokemon_abilities.ability_id 
-            INNER JOIN pokemon ON pokemon.id = pokemon_abilities.pokemon_id
-            WHERE pokemon_abilities.pokemon_id="{aID}"'''.\
-            format(aID=id))
-        abilities = c.fetchall()
-        print(abilities)
-        for row in abilities:
-            print(row)
+    # Display abilities
+    needAbil = ''
+    if(table == 'pokemon'):
+        needAbil = input("Would you like to see their abilities? ")
+        formatHeader = [] 
+        for item in headerList:
+            tempList = item.split('.')
+            if(item == 'abilities.identifier'):
+                formatHeader.append('Ability ID')
+            else:
+                formatHeader.append(tempList[1])
+    if(needAbil == 'y' or needAbil == 'Y' or needAbil == 'yes' or needAbil == 'Yes' or needAbil == 'YES'):
+        pokeTable = PrettyTable(formatHeader)
+        for id in pokemonID:
+            # Grab the pokemon name and ability name using the id #'s from pokemon_abilities
+            #c.execute('''SELECT pokemon.identifier, abilities.identifier
+            c.execute('''SELECT pokemon.identifier, pokemon.height, pokemon.weight, abilities.identifier
+                FROM pokemon_abilities
+                INNER JOIN abilities ON abilities.id = pokemon_abilities.ability_id 
+                INNER JOIN pokemon ON pokemon.id = pokemon_abilities.pokemon_id
+                WHERE pokemon_abilities.pokemon_id="{aID}"'''.\
+                format(aID=id))
+            abilities = c.fetchall()
+            for row in abilities:
+                #pokemonAList.append(row[0])
+                #abilityList.append(row[1])
+                pokeTable.add_row(row)
+            print(pokeTable)
         
-    tabstring = x.get_string
-    
+
     #Close SQL connection
     conn.close()
+    
+# Call queryDB function providing all variables (even if not used) function validates input.
 queryDB(qTable, qColumn, qid)
