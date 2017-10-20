f04a90d010efb43609dc2647d0963fa60e11e5c4
Zach Bresser <ObamaNYoMama@users.noreply.github.com>
2017-10-20 16:41:56 -0400

    Initial Upload
    WIP, can query all tables, with formatting, and if querying Pokemon table it will also grab their Special Abilities including Hidden Abilities, but does not display it in a clear manner, yet...

---
+++ main.py
@@ -0,0 +1,65 @@
+import sqlite3
+from prettytable import PrettyTable
+
+while True:
+    qTable = input("What table would you like?")
+    if(qTable):
+        break
+    else:
+        print("You must enter a table!!")
+qColumn = input("Which column are we querying?")
+qid = input("What are we looking for?")
+
+
+def queryDB(table, column, id):
+
+    # Declare Variables
+    sqlite_file = 'veekun-pokedex.sqlite'
+    queried_table = table
+    id_column = column
+    some_id = id
+    listHeaders = []
+    pokemonID = []
+
+    # Connect to SQLite3 Database
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+
+    c.execute("PRAGMA table_info({tn})".\
+        format(tn=queried_table))
+    columns = c.fetchall()
+    columns = list(columns)
+    
+    # Query and format output
+    for column in columns:
+        listHeaders.append(column[1])
+
+    if(qTable and qColumn and qid):
+        c.execute("SELECT * FROM {tn} WHERE {cn} LIKE '{id}'".\
+            format(tn=queried_table, cn=id_column, id=some_id))
+        all_rows = c.fetchall()
+    else:
+        c.execute("SELECT * FROM {tn}".\
+            format(tn=queried_table))
+        all_rows = c.fetchall()
+    x = PrettyTable(listHeaders)
+    # Add all rows to table
+    for row in all_rows:
+        x.add_row(row)
+        if(table == "pokemon"):
+            pokemonID.append(row[0])
+    
+    print(x)
+    
+    print("Abilities: ")
+    for id in pokemonID:
+        c.execute("SELECT abilities.identifier FROM pokemon_abilities INNER JOIN abilities ON abilities.id = pokemon_abilities.ability_id WHERE pokemon_abilities.pokemon_id='{aID}'".\
+            format(aID=id))
+        abilities = c.fetchall()
+        print(abilities)
+        
+    tabstring = x.get_string
+    
+    #Close SQL connection
+    conn.close()
+queryDB(qTable, qColumn, qid)
