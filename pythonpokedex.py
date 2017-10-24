1a046c0932520745e82e94846b9b44f9ddeb9c40
Unknown <zachbresser@gmail.com>
2017-10-24 19:07:52 -0400

    Formatting and page design
    Decided on bootstrap as the web framework, started bootstrap design of pokemon.html
    
    Currently uses a page header for pokemon img and name, table that is responsive and highlights hovered row (not likely to stick with table) for general pokemon information, and then uses panel+list-group for base stats.

--- pythonpokedex.py
+++ pythonpokedex.py
@@ -18,23 +18,41 @@ def pokemon(name):
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
     
+    # PRAGMA retrieves the table schema, I then in the template pokemon.html iterate through
+    # and grab just the headers from the query.
     c.execute("PRAGMA table_info(pokemon)")
     columns = c.fetchall()
     
+    # Currently gets all information from the pokemon table using the name retreived from the 
     c.execute("SELECT * FROM pokemon WHERE identifier='{pid}'".\
         format(pid=name))
     
     all_rows = c.fetchall()
+    
+    # Grab pokemon_id from query and convert to string
     pokemon_id = all_rows[0][0]
     pokemon_id = str(pokemon_id)
+    
+    # Grab base stats before converting pokemon_id to long notation
+    c.execute('''SELECT stats.identifier, pokemon_stats.base_stat
+                 FROM pokemon_stats
+                 INNER JOIN stats ON pokemon_stats.stat_id = stats.id
+                 WHERE pokemon_stats.pokemon_id="{sID}"'''.\
+          format(sID=pokemon_id)) 
+    base_stats = c.fetchall()
+    
+    # Determine Length of pokemon_id, if 1 digit add two 0's at beginning, or 2 add 1 0, etc
+    # This is used in the template to hotlink from Nintendo's site, is not a viable solution. Will need to host profiles later on
     if(len(pokemon_id) == 1):
         pokemon_id = "00" + pokemon_id
     elif(len(pokemon_id) == 2):
         pokemon_id = "0" + pokemon_id
         
+    # Close SQL connection when done    
+    conn.close()
     return render_template(
-        'name.html', **locals())
+        'pokemon.html', **locals())
         
-# Autorun the app when this python script is called. 
+# Autorun the app on 0.0.0.0 when this python script is called. 
 if __name__ == "__main__":
-        app.run()
+        app.run(host='0.0.0.0')
